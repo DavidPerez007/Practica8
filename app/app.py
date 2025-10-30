d0331a42c3e09202830722064252aa5ab17bf5d9
david <davipcleo@gmail.com>
2025-10-30 16:19:31 -0600

    feat: added port number for deploy

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import os
 from models.rf_nuestro import RandomForest 
 import queue
 import threading
@@ -123,5 +124,6 @@ def info():
 
     return jsonify(result), 200
 
-if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False, threaded=True)
